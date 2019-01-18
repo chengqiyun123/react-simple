25f3ebfae4d62bc86f02498245bbb9a6035c3189
chengqy <1209157062@qq.com>
2019-01-18 20:08:37 +0800

    This is a simple react framework

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../common/css/common.css';
+import "../common/icon/iconfont.css"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello world
+      </div>
+    );
+  }
+}
+
+export default App;
